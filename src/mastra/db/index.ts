152e16fe7add7fef88e99f837eac01d0df4a3266
simon200418 <simonpathre20@gmail.com>
2026-06-06 19:48:51 +0530

    Final Project

--- src/mastra/db/index.ts
+++ src/mastra/db/index.ts
@@ -3,12 +3,20 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// export const pool = new Pool({
+//   host: process.env.DB_HOST || "localhost",
+//   port: Number(process.env.DB_PORT) || 5432,
+//   user: process.env.DB_USER || "postgres",
+//   password: process.env.DB_PASSWORD || "1234",
+//   database: process.env.DB_NAME || "provue_tara",
+// });
+
+
 export const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  port: Number(process.env.DB_PORT) || 5432,
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "1234",
-  database: process.env.DB_NAME || "provue_tara",
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export async function initDB() {
